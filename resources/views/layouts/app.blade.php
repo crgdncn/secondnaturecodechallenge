191499b0fbc7693c7e090a59d84a877cc3f6b54e
Craig Duncan <cduncan@filtereasy.com>
2019-11-25 11:14:33 -0500

    refactored logout menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -52,22 +51,14 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('customer.show', [Auth::user()->id], false) }}">{{ __('Home') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <form id="logout-form" method="POST" action="{{route('logout', [], false)}}">
+                                    @csrf
+                                </form>
+                                <a class="nav-link"  onclick="logout(); return false;">{{ __('Logout') }}</a>
                             </li>
                         @endguest
                     </ul>
@@ -79,5 +70,10 @@
             @yield('content')
         </main>
     </div>
+    <script type="text/javascript">
+        function logout() {
+            document.getElementById('logout-form').submit();
+        }
+    </script>
 </body>
 </html>
