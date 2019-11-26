dea8b2abbba6fb9cec006e6fe216f42585eb43a3
crgdncn <crgdncn@gmail.com>
2019-11-25 20:32:17 -0500

    wip

---
+++ resources/views/customer/partials/widget_row.blade.php
@@ -0,0 +1,11 @@
+                        <tr>
+                            <td>
+                                sku-num
+                            </td>
+                            <td>
+                                My name
+                            </td>
+                            <td>
+                                Delete
+                            </td>
+                        </tr>
--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -6,10 +6,34 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <span class="customer-name">{{ $customer->first_name . ' ' . $customer->last_name }}</span>
+                    <span class="customer-name">
+                        {{ $customer->first_name . ' ' . $customer->last_name }}
+                    </span>
                     <a class="ml-2 edit-details" href="{{ route('customer.edit', [$customer->id], false) }}">(edit account details</a>)
                 </div>
-                // show widgets here
+
+                <h5 class="ml-2 mt-2">My Widgets<h5>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th class="th">
+                                SKU
+                            </th>
+                            <th class="th">
+                                Name
+                            </th>
+                            <th>
+                                <a class="add-widget" href="#">Add Widget</a>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($customer->widgets as $widget)
+                            @include('customer.partials.widget_row')
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -20,11 +44,24 @@
      <style>
         .customer-name {
             font-weight: bold;
-            font-size: 1.1rem;
+            font-size: 1.3rem;
         }
 
         .edit-details {
             font-size: 0.8rem;
+        }
+
+        .th {
+            font-size: 0.8rem;
+            font-weight: bold;
+        }
+
+        td {
+            font-size: 0.7rem;
+        }
+
+        .add-widget {
+            font-size: 0.6rem;
         }
     </style>
 @endpush
