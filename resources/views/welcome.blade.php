01b364b56ebda45df63e229c56513ef7b0baec63
crgdncn <crgdncn@gmail.com>
2019-12-01 23:24:42 -0500

    remove preious limititstions to get app stated from welcome page instructions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
                     </p>
                     <p>
                         A customer/admin *must* register by themselves, they cannot be created by an admin. However to become an admin, another admin must grant a customer admin permissions. Seed the database (php artisan db:seed) to create the first admin user [admin@sncc.test / password]. <br><br>
-                        All other registered users *must* verify their email address or they will not be able to access the full functionality of the site. [mailtrap.io anyone?]
                     </p>
                     <p>
                         Admins and only admins can CRUD widgets. <br>
@@ -28,12 +27,6 @@
                     </p>
                     <p>
                         This is *not* a shopping cart application.
-                    </p>
-                    <p>
-                        There is a minimal amount of configuration needed to get this application to run, but it is essential for testing that a mailtrap or equivalent account be used to manage multiple customer/admin registration verifications. Sorry, but if I have been asked to put this much effort in, a little effort on your behalf is not too much to ask in return.
-                    </p>
-                    <p>
-                        Note: due to time constraints, no effort to style this application beyond the default Laravel install CSS has been made (hence the reason why mobile hamburger menu does not function correctly).
                     </p>
                 </div>
             </div>
